0d968be62d59cd28afd2b49dfef76fa438260c14
z5531794 <z5531794@vx14.cse.unsw.EDU.AU>
2026-07-20 01:08:46 +1000

    Initilize the event, type and states enum

--- SmartDoor/Core/Inc/fsm.h
+++ SmartDoor/Core/Inc/fsm.h
@@ -8,4 +8,36 @@ typedef enum {
     DIR_EXIT
 } Direction_t;
 
+
+typedef enum {
+    IDLE,
+    AUTHORISED,
+    PASSAGE,
+    ALERT,
+    CLOSING,
+    ADMIN,
+    UNLOCKED
+} DoorState_t;
+
+typedef enum {
+    CARD_NONE,
+    CARD_INVALID,
+    CARD_NORMAL,
+    CARD_ADMIN
+} CardType_t;
+
+typedef enum {
+    EVT_NONE,
+    EVT_CARD_SCANNED,
+    EVT_LDR_FRONT,
+    EVT_LDR_REAR,
+    EVT_PASSAGE_DONE,
+    EVT_TAILGATE_DETECTED,
+    EVT_TIMEOUT,
+    EVT_KEYPAD_KEY,
+    EVT_ADMIN_EXIT,
+    EVT_SCHEDULE_START,
+    EVT_SCHEDULE_END
+} Event_t;
+
 #endif
